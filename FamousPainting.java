c2ae0a1bc37ee20882806db3868a665ef4f2b05c
Mahyar Matin Azad <mrmatinazad@mail.lipscomb.edu>
2016-11-08 19:51:09 -0600

    famous paintings

---
+++ FamousPainting.java
@@ -0,0 +1,11 @@
+/**
+ * Created by MikaMatin on 11/8/16.
+ */
+
+public class FamousPainting extends Painting {
+    public FamousPainting(String t, String a) {
+        super(t, a);
+        price = 25000;
+    }
+}
+
